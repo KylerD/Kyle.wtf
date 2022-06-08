547044008d23d0fcd0512d17e392adc14b91fe60
Kyle Davidson <k.davidson@kainos.com>
2022-06-08 15:23:14 +0100

    Integrate tailwind and cn

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import React from 'react'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+export const name = '< Kyle Davidson />'
+export const siteTitle = '<Kyle />'
 
 export default function Layout({
   children,
@@ -15,65 +13,33 @@ export default function Layout({
   home?: boolean
 }) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="Kyle Davidson's website"
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <title>{siteTitle}</title>
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
+      {!home && (
+        <>
+          <header className='flex flex-row flex-nowrap w-full'>
             <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
+              <h1 className='text-xl font-bold text-green-500 hover: cursor-pointer'>{name}</h1>
             </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+          </header>
+        </>
+      )}
       <main>{children}</main>
       {!home && (
-        <div className={styles.backToHome}>
+        <div >
           <Link href="/">
             <a>← Back to home</a>
           </Link>
         </div>
       )}
-    </div>
+    </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Layout, { name } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
+import Image from 'next/image'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import cn from 'classnames';
+import React from 'react'
 
 export default function Home({
   allPostsData
@@ -15,28 +16,154 @@ export default function Home({
     id: string
   }[]
 }) {
+
+  const [expand, setExpand] = React.useState(false);
+
+  function handleExpand() {
+    setExpand(!expand);
+  }
+
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+
+      <header className='flex flex-row flex-wrap w-full h-screen justify-center items-center'>
+        <div className={cn('flex flex-row justify-center items-center w-full', { 'md:w-1/2': expand })}>
+          <button onClick={handleExpand}
+            className={cn('text-6xl font-bold text-green-500 animate-pulse hover:cursor-pointer hover:animate-none', { 'animate-none': expand })}>{name}</button>
+        </div>
+
+        <div className={cn("w-full md:w-1/2 p-4", { 'hidden': !expand })}>
+          <div className="w-full flex-row flex-wrap">
+            {/* About stack */}
+            <div className="relative col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
+              <div className="relative flex text-slate-400 text-xs leading-6">
+                <div className="font-['terminal'] mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">
+                  about (node)
+                </div>
+                <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden">
+                  <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
+                </div>
+
+              </div>
+              <div className="relative">
+                <pre className="text-sm leading-6 text-slate-50 flex ligatures-none">
+                  <code className="flex-none min-w-full p-5">
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-pink-400 w-auto h-6 mr-3">
+                        <path d="M0 0L3 3L0 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        Building website...
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        .....TYPESCRIPT (<span className='text-green-400'>100%</span>)
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        .....NEXT.JS (<span className='text-yellow-400'>70%</span>)
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="flex-auto font-['terminal']">
+                        .....TAILWIND (<span className='text-red-400'>30%</span>)
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="flex-auto font-['terminal']">
+                        <span className='text-green-400'>COMPLETE</span>
+                      </p>
+                    </div>
+                    <div className="flex my-2">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-pink-400 w-auto h-6 mr-3">
+                        <path d="M0 0L3 3L0 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        Shilling socials...
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        .....EMPLOYER: <span className='text-green-400 underline'>
+                          <Link href={'https://www.kainos.com/'}>KAINOS</Link>
+                        </span>
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        .....TWITTER: <span className='text-blue-400 underline'>
+                          <Link href={'https://twitter.com/iKyleDavidson'}>TWEETING</Link>
+                        </span>
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="font-['terminal'] flex-auto">
+                        .....LINKEDIN: <span className='text-blue-400 underline'>
+                          <Link href={'https://www.linkedin.com/in/kyledavidson200/'}>LINKED</Link>
+                        </span>
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <svg viewBox="0 -9 3 24" aria-hidden="true" className="flex-none overflow-visible text-green-400 w-auto h-6 mr-3">
+                        <line x1="0" y1="5" x2="5" y2="5" stroke="currentColor" />
+                      </svg>
+                      <p className="flex-auto font-['terminal']">
+                        <span className='text-green-400'>Scroll down for more</span>
+                      </p>
+                    </div>
+                  </code>
+                </pre>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+
+      <section className="flex flex-row flex-wrap justify-center w-full">
+        <h1 className='text-4xl font-bold w-full text-center'>Bout Ye</h1>
+
+        <div className='flex w-full mx-auto justify-center my-2'>
+          <Image className='rounded-full' priority src="/images/kd.jpeg" width='100' height='100' />
+        </div>
+
+        <p className="text-black text-xl font-semibold text-center mx-auto my-2">
+          Hey! I'm Kyle, I work as a Solution Architect at Kainos.
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section>
+        <h2 className="text-3xl font-bold underline">Blog</h2>
+        <ul>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small>
                 <Date dateString={date} />
               </small>
             </li>
