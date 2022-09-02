48e597254c4d682ba1d937541ce3a88fee4c6984
Kyle Davidson <kyle.d@live.co.uk>
2022-09-02 13:35:32 +0100

    Social media preview

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 export const name = '< Kyle Davidson />'
 export const siteTitle = '<Kyle />'
+export const webUrl = 'https://www.kyled.wtf/'
 
 export default function Layout({
   children,
@@ -18,10 +19,15 @@ export default function Layout({
         <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="all" key="robots" />
         <meta
-          name="description"
-          content="Kyle Davidson's website" key="description"
+          name="og:description"
+          content="Blogs and stuff" key="description"
         />
         <meta name="og:title" content={siteTitle} key="title" />
+        <meta name="og:url" content={webUrl} key="url" />
+        <meta name="og:type" content="website" key="type" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="og:image" content="favicon.ico" />
+
         <title>{siteTitle}</title>
       </Head>
 
